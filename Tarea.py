5bf73f76e4b2dc44ac4134a6f7ae5fbf59970418
Ander <And@outlook.com>
2017-06-27 16:08:08 -0600

    Primera Tarea

---
+++ Tarea.py
@@ -0,0 +1,7 @@
+user = input(' Ingresar el nombre de la persona: ')
+old = int(input('Ingresar la edad de la persona: '))
+anho = int(input('Ingresar el año actual del mundo: '))
+resta = 100 - old
+suma = resta + anho
+print('En al año: ', suma, user, 'cumplira 100 años de haber nacido')
+print(' <(*) ')
