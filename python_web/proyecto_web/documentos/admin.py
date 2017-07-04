bff908929fd8eca2357b4dde0c13d94a7e930d9c
Ander <And@outlook.com>
2017-07-04 17:51:36 -0600

    actualizado

--- python_web/proyecto_web/documentos/admin.py
+++ python_web/proyecto_web/documentos/admin.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+# Importar modelo
+from documentos.models import Documentos
+# Crear una sub-clase de admin.ModelAdmin
+class DocumentoAdmin(admin.ModelAdmin):
+	model = Documentos
+
+# Registrar ModelAdmin para C/modelo
+admin.site.register(Documentos, DocumentoAdmin)
