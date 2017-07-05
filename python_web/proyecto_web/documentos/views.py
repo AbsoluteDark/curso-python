d7cdb9b538ad525d0d0414a2b03614d049c1a3b8
Ander <And@outlook.com>
2017-07-05 17:53:22 -0600

    Lista de HTML

--- python_web/proyecto_web/documentos/views.py
+++ python_web/proyecto_web/documentos/views.py
@@ -4,3 +4,27 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+# Las vistas manejan la logica del sitio web.
+from django.views.generic import View
+
+# Metodo render retorna una respuesta
+# Combina una plantilla con un diccionary of contexto
+# Y retorna un objeto HttpResponse
+from django.shortcuts import render
+from documentos.models import Documentos as ModeloDocumentos
+
+class Documentos(View):
+	def get(self, request, *args, **kwargs):
+		
+		docs = ModeloDocumentos.objects.all()
+		context = {
+		'docs': docs,
+		'encabezado': 'Mys owns Documents'
+		}
+
+		return render(
+			request, 
+			'documentos.html', 
+			context
+			)
+	
